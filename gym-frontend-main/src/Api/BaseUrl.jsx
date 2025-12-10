582a7d6d6e2f49a0d77a47a43024fcf848e50d15
Rohit Patidar <ritikpatidar580@gmail.com>
2025-12-10 19:20:32 +0530

    rohit

--- gym-frontend-main/src/Api/BaseUrl.jsx
+++ gym-frontend-main/src/Api/BaseUrl.jsx
@@ -1,12 +1,9 @@
  
-const BaseUrl = 'https://02x4fc84-4000.inc1.devtunnels.ms/api/';
+// const BaseUrl = 'https://02x4fc84-4000.inc1.devtunnels.ms/api/';
 
-<<<<<<< HEAD
-const BaseUrl = 'https://02x4fc84-4000.inc1.devtunnels.ms/api/';
+// const BaseUrl = 'https://02x4fc84-4000.inc1.devtunnels.ms/api/';
 
-=======
->>>>>>> 82ab658101064e06e894e8eaebd1a4e9fe44df53
-// const BaseUrl = 'https://gymnew-backend-5-dec-production.up.railway.app/api/';
+const BaseUrl = 'https://gymnew-backend-5-dec-production.up.railway.app/api/';
 
 export default BaseUrl;
 
