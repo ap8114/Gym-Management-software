7aafd5174fdc35a79e9727f362f0f4661443d247
Ritika Pirag <120805086+veniyadav@users.noreply.github.com>
2025-12-10 18:35:46 +0530

    ritika

--- gym-frontend-main/src/Api/BaseUrl.jsx
+++ gym-frontend-main/src/Api/BaseUrl.jsx
@@ -1,6 +1,6 @@
 
 // const BaseUrl = 'https://0wljd5bz-4000.inc1.devtunnels.ms/api/';
-// const BaseUrl = 'https://0wljd5bz-4000.inc1.devtunnels.ms/api/';
+// const BaseUrl = 'https://02x4fc84-4000.inc1.devtunnels.ms/api/';
 
 
 const BaseUrl = 'https://gymnew-backend-5-dec-production.up.railway.app/api/';
