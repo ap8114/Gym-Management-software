e4b7a7b8367a23145f063af0addc90250dc2d861
divyagupta7 <divyaguptaw@gmail.com>
2025-12-15 12:26:51 +0530

    changes

--- gym-frontend-main/src/Layout/DynamicPage.jsx
+++ gym-frontend-main/src/Layout/DynamicPage.jsx
@@ -497,7 +497,7 @@ const DynamicPage = () => {
   if (!settings) {
     return (
       <div className="text-center mt-5">
-        <h3>Page Not Found</h3>
+        {/* <h3>Page Not Found</h3> */}
       </div>
     );
   }
